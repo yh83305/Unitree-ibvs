65e065cad315f56145c2db63b5306184cef53eef
yh83305 <203651337@qq.com>
2025-03-18 16:07:23 +0800

    second

--- build/unitree_ros/robots/go1_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/unitree_ws/src/unitree_ros/robots/go1_description
-# Build directory: /home/ubuntu/unitree_ws/build/unitree_ros/robots/go1_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
